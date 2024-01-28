a1a26e30a99e784928db26ca5dad788be0adde43
Josy Dom Alexis <josyanimator@gmail.com>
2024-01-28 16:29:05 +0530

    component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useContext, useEffect, useState } from 'react';
 import { CastleContext, ThemeContext } from './index.js';
+import Nav from "./Nav.jsx"
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+      <h3>Navbar</h3>
+    </div>
+  )
+}
